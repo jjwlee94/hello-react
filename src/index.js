6258fd62271a44175b3b0a4f1059007b7f651571
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-06 12:51:59 -0500

    Add an input field

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const Application = () => {
 
   return (
     <main>
+      <input placeholder="Type your name" />
       <Button buttonText={"RESET"} clickButton={reset} />
       <h1>Hello {name}</h1>
     </main>
